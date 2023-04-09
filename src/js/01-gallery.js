474757f419da1631e510d2490e5c0506a7c077cb
Katerina <118116963+NonStopIT17@users.noreply.github.com>
2023-04-09 21:11:39 +0300

    Home work 8
    Add js gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imageMarkup = creatSrtElmGallImg(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imageMarkup);
+
+function creatSrtElmGallImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" onclick="event.preventDefault()"
+     href="${original}">
+  <img class="gallery__image"
+   src="${preview}"
+    alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.7,
+});
